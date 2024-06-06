c63aa57b9bc5d295026d4c89a6026df2be82a80e
jcst2019 <juansolar_4@hotmail.com>
2024-06-05 20:50:46 -0500

    commint Inicial

---
+++ src/main/java/com/reto/indra/ms_retoindra_customer_backend/MsRetoindraCustomerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.reto.indra.ms_retoindra_customer_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsRetoindraCustomerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsRetoindraCustomerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reto/indra/ms_retoindra_customer_backend/MsRetoindraCustomerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reto.indra.ms_retoindra_customer_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsRetoindraCustomerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
