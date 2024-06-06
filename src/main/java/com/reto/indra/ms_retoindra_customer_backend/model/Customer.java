09b18371e4e6dce9e084b57e88c60822ec1b485f
jcst2019 <juansolar_4@hotmail.com>
2024-06-06 07:56:34 -0500

    Agregar capa repository

--- src/main/java/com/reto/indra/ms_retoindra_customer_backend/model/Customer.java
+++ src/main/java/com/reto/indra/ms_retoindra_customer_backend/model/Customer.java
@@ -3,12 +3,19 @@ package com.reto.indra.ms_retoindra_customer_backend.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.domain.Persistable;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-public class Customer {
-
+@Table(value="customers")
+public class Customer implements Persistable<Long> {
+    @Id
+    @Column(value="idcustomer")
     private Long id;
     private String uniqueCode;
     private String name;
@@ -18,5 +25,16 @@ public class Customer {
     private String address;
     private String phoneNumber;
     private String email;
+    @Transient
+    private boolean nuevo;
 
+    @Override
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public boolean isNew() {
+        return nuevo;
+    }
 }
